6f303765783214cb0dd4daff6aae9e0b05601b20
Yuan Yang <cyberyuanyang@gmail.com>
2018-03-13 16:25:48 +0800

    Fix trie bug

--- trie.cpp
+++ trie.cpp
@@ -22,14 +22,15 @@ public:
         root_(unique_ptr<TrieNode>(new TrieNode))
     {
     }
+
     void insert(const string& s)
     {
-        int len = s.length();
-        if (len <= 0) return;
+        int n = s.length();
+        if (n <= 0) return;
 
-        for (int i = 0; i < len; i++)
+        for (int i = 0; i < n; i++)
         {
-            TrieNode* node = _find(s, i, len-i+1);
+            TrieNode* node = _find(s, i, n-i);
             node->val = s;
         }
     }
@@ -41,14 +42,14 @@ public:
 
         for (int i = 0; i < n; i++)
         {
-            TrieNode* node = _find(s, i, n-i+1);
+            TrieNode* node = _find(s, i, n-i);
             node->val.clear();
         }
     }
 
-    int find(const string& s, vector<string> result, int result_n)
+    int find(const string& s, vector<string> &result, int result_n)
     {
-        TrieNode* node = _find(s, 0, s.length()-1);
+        TrieNode* node = _find(s, 0, s.length());
         if (!node->val.empty()) 
         {
             result.push_back(node->val);
@@ -67,25 +68,34 @@ public:
         return result.size();
     }
 
+    void debug_print()
+    {
+        cout << "root:" << root_.get() << root_->val << endl;
+        _debug_print(root_.get(), "");
+    }
+
 private:
     TrieNode* _find(const string& s, int index, int len)
     {
         TrieNode *node = root_.get();
-        for (int i = 0; i<len; i++) 
+        for (int i = 0; i < len; i++)
         {
             char c = s[index+i];
             if (!node->children[c])
             {
                 node->children[c] = unique_ptr<TrieNode>(new TrieNode);
-                node = node->children[c].get();
             }
+
+            node = node->children[c].get();
         }
+
         return node;
     }
 
-    void _search_leaf(TrieNode* node, vector<string> result, int& left)
+    void _search_leaf(TrieNode* node, vector<string> &result, int &left)
     {
-        if (left <= 0) return;
+        if (left <= 0) 
+            return;
 
         if (!node->val.empty()) 
         {
@@ -98,8 +108,20 @@ private:
             {
                 TrieNode* node = kv.second.get();
                 _search_leaf(node, result, left);
-                if (left <= 0) return;
+                if (left <= 0) 
+                    return;
             }
+        }
+    }
+
+    void _debug_print(TrieNode *node, string blank)
+    {
+        blank.append("    ");
+        for (auto& kv : node->children)
+        {
+            TrieNode *node = kv.second.get();
+            cout << blank << kv.first << ":" << node->val << "@" << node << endl;
+            _debug_print(kv.second.get(), blank);
         }
     }
 
@@ -113,11 +135,22 @@ int main()
 {
     enjoy::Trie trie;
     trie.insert("hello world");
+    trie.insert("hello world 1");
+    trie.debug_print();
+
     vector<string> result;
-    cout << trie.find("hello", result, 1) << endl;
+    cout << trie.find("hello world", result, 1) << endl;
+    for (auto& v : result)
+    {
+        cout << "::::" << v << endl;
+    }
 
     trie.erase("hello world");
 
     vector<string> result1;
-    cout << trie.find("hello", result1, 1) << endl;
+    cout << trie.find("hello world", result1, 1) << endl;
+    for (auto& v : result1)
+    {
+        cout << "::::" << v << endl;
+    }
 }
